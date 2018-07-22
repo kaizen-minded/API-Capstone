630995540f4f35f58b9a14617fb591e58e16df32
Melvin Ezell <melvin3ezell@gmail.com>
2018-07-22 17:03:16 -0400

    reformated the js code

--- main.js
+++ main.js
@@ -2,96 +2,134 @@
 
 $(function(){
 
+	let formRequest = {
+		getValue: function(selector){
+			return $(`${selector}`).val();
+		},
+		createDistancePercent: function(selector){
+			let distance = this.getValue(selector);
+			if(distance < 1){
+				distance = 1;
+			} 
+			return distance / 100;
+		}
+	};
+	let formResponse= {
+		changeMeetUpValue: function(silderValue, selector){
+			let slider1 = parseInt(silderValue, 10);
+			let dynamicResponse = "";
+			if(slider1 >= 45 && slider1 <= 55){
+				dynamicResponse = "somewhere in the middle";
+			} else if(slider1 < 1){
+				dynamicResponse = "closer to my place";
+			}else if (slider1  < 44){
+				dynamicResponse = "at my place";
+			} else if(slider1 === 100){
+				dynamicResponse = "at your place";
+			}
+			else{
+				dynamicResponse = "closer to your place";
+			}
+			$(`${selector}`).text(dynamicResponse);
+			$("#distance-percent").text(slider1);
+		},
+		findMiddleGround: function(start, end){
+			let directionsService = new google.maps.DirectionsService();
+			let directionsRequest = {
+			origin: start,
+			destination: end,
+			travelMode: google.maps.DirectionsTravelMode.DRIVING
+				};
+			return [directionsService, directionsRequest];
+		},
+		googleResponse: function (response, status) {
+			if (status == google.maps.DirectionsStatus.OK) {
+				startPoint = response.routes[0].legs[0].start_location;
+				endPoint = response.routes[response.routes.length-1].legs[0].end_location;
+				let percentDistance = formRequest.createDistancePercent("#distance-between");
+				let userQuery = formRequest.getValue("#user-query");
+				let pos = google.maps.geometry.spherical.interpolate(startPoint,endPoint, percentDistance);
+				let lat = pos.lat();
+				let lng = pos.lng();
+				formResponse.findLocalSpots(lat, lng, userQuery)
+			}
+		},
+		findLocalSpots: function(latitude, longitude, userQuery){
+			var coordinates = new google.maps.LatLng(latitude,longitude);
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: coordinates,
+				zoom: 13
+				});
+			let request = {
+				location: coordinates,
+				radius: '500',
+				query: userQuery,
+			};
+			service = new google.maps.places.PlacesService(map);
+			service.textSearch(request, formResponse.callback);
+		},
+		callback: function(results, status){
+			if (status == google.maps.places.PlacesServiceStatus.OK) {
+				console.log(results);	
+				$("#query-results").text("");
+				for (let i = 0; i < results.length; i++) {
+				let finalresults = "";
+				let place = results[i];
+				let newplace = `<article>
+				<div class="details">
+					<div class="col-6">
+						<h3 id="query-title">${place.name}</h3>
+					</div>
+					<div class="col-6" class="query-info">
+						<h4>Rating: <span class="query-rating">${place.rating}</span><i class="fas fa-star"></i></h4>
+						<h4>Price: <span>${formResponse.setNumberOfDollar(place.price_level, i)}</span></h4>
+						<h3>${place.formatted_address}</h3>		
+							</div>
+						</div>
+					</article>
+				`
+				finalresults += newplace;
+				$("#query-results").append(finalresults);
+				marker = new google.maps.Marker({ position: place.geometry.location, map: map, title: place.name });
+				}
+			}
+
+		},
+		setNumberOfDollar: function(number, index){
+			let dollarsigns = "";
+			if(number){
+				for(let i = 0; i < number; i++){
+					dollarsigns += `<i class="fas fa-dollar-sign"></i>`;
+				}
+			}else{
+				dollarsigns = "No Data"
+			}
+			return dollarsigns;
+		}
+
+		}
+
+
 	$("#distance-between").change(function(){
-		let slider1 = parseInt($("#distance-between").val(), 10);
-		if(slider1 >= 45 && slider1 <= 55){
-			$("#meet-up-text").text("somewhere in the middle");
-		} else if(slider1 < 1){
-			$("#meet-up-text").text("closer to my place");
-		}else if (slider1  < 44){
-			$("#meet-up-text").text("at my place");
-		} else if(slider1 === 100){
-			$("#meet-up-text").text("at your place");
-		}
-		else{
-			$("#meet-up-text").text("closer to your place");
-		}
-		$("#distance-percent").text(slider1);
+			let distanceBetween = formRequest.getValue("#distance-between");
+			formResponse.changeMeetUpValue(distanceBetween, "#meet-up-text");
 	});
 
 	$("form").submit(function(e){
 		e.preventDefault();
-		let myAddress = getAddress("#my-address");
-		let yourAddress =getAddress("#your-address");
-		let percentDistance = getPercentDistance();
-		findMiddleGround(myAddress, yourAddress, percentDistance);
-		console.log(myAddress, yourAddress)
+		let myAddress = formRequest.getValue("#my-address");
+		let yourAddress = formRequest.getValue("#your-address");
+		let [directionsService, directionsRequest] = formResponse.findMiddleGround(myAddress, yourAddress);
+		directionsService.route(directionsRequest, formResponse.googleResponse);
 	});
 
-	function getAddress(element){
-		let address = $(`${element}`).val()
-		return address
-	};
-
-	function getPercentDistance(){
-		let distance = $("#distance-between").val();
-		if(distance < 1){
-			distance = 1;
-		} 
-		return distance / 100;
-	}
-
-	function findMiddleGround(start, end, percentDistance){
-		let directionsService = new google.maps.DirectionsService();
-		let directionsRequest = {
-			origin: start,
-			destination: end,
-			travelMode: google.maps.DirectionsTravelMode.DRIVING,
-		
-		};
-		directionsService.route(directionsRequest, function (response, status) {
-			if (status == google.maps.DirectionsStatus.OK) {
-				startPoint = response.routes[0].legs[0].start_location;
-				endPoint = response.routes[response.routes.length-1].legs[0].end_location;
-				let pos = google.maps.geometry.spherical.interpolate(startPoint,endPoint, percentDistance);
-				console.log(pos.lat(), pos.lng());
-				let lat = pos.lat();
-				let lng = pos.lng();
-				initialize(lat, lng)
-			}
-		})
-	}
-
-	function initialize(latitude, longitude) {
-		var pyrmont = new google.maps.LatLng(latitude,longitude);
-	  
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: pyrmont,
-			zoom: 15
-		  });
-	  
-		var request = {
-		  location: pyrmont,
-		  radius: '500',
-		  query: 'restaurant',
-		};
-	  
-		service = new google.maps.places.PlacesService(map);
-		service.textSearch(request, callback);
-	  }
-	  
-	  function callback(results, status) {
-		if (status == google.maps.places.PlacesServiceStatus.OK) {
-			console.log(results);	
-		  for (var i = 0; i < results.length; i++) {
-			var place = results[i];
-			if(results[i].photos){
-				console.log(results[i].photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200}));
-			}
-			marker = new google.maps.Marker({ position: place.geometry.location, map: map });
-		  }
-		}
-	  }
+		// function setNumberOfDollar(number, index){
+		// 	let dollarsigns = "";
+		// 	for(let i = 0; i < number; i++){
+		// 		dollarsigns += `<i class="fas fa-dollar-sign"></i>`;
+		// 	}
+		// 	return dollarsigns;
+		// }
 	
 
 
